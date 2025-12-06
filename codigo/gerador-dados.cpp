1f767fbe8d9ada6fb488fc295f03e65d1db63b47
anamacowski <anamacowski@alunos.utfpr.edu.br>
2025-12-06 00:34:18 -0300

    Gerador com arquivos

--- codigo/gerador-dados.cpp
+++ codigo/gerador-dados.cpp
@@ -2,13 +2,15 @@
 #include <cstdlib>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
 /*
-TAM       CENÁRIO
+TAM            CENÁRIO
 P: 1.000       A: aleatório
 M: 10.000      C: crescente
 G: 100.000     D: decrescente
 */
+
+/*--------------- FUNÇÕES AUXILIARES ---------------*/
 
 int tratamento_tamanho(char tam){
     int n;
@@ -23,7 +25,7 @@ int tratamento_tamanho(char tam){
             break;
 
         case 'G':
-            n = 10000;
+            n = 100000;
             break;
 
         default:
@@ -34,59 +36,114 @@ int tratamento_tamanho(char tam){
 }
 
 
-void gerador_arquivo(int tam, char cenario){
-    int *vetor = (int *)malloc(tam * sizeof(int)); 
+const char* obter_nome_tam(char tam) {
+    if (tam == 'P') {
+        return "pequeno";
+    } 
+    
+    else if (tam == 'M') {
+        return "medio";
+    } 
+    
+    else if (tam == 'G') {
+        return "grande";
+    }
 
+    return "desconhecido"; 
+}
+
+
+const char* obter_nome_cenario(char cenario) {
+    if (cenario == 'A') {
+        return "aleatorio";
+    } 
+    
+    else if (cenario == 'C') {
+        return "crescente";
+    } 
+    
+    else if (cenario == 'D') {
+        return "decrescente";
+    }
+    
+    return "desconhecido"; 
+}
+
+
+
+/*--------------- FUNÇÃO GERADORA ---------------*/
+
+void gerador_arquivo(char cod_tam, char cod_cenario){
+    int tam = tratamento_tamanho(cod_tam);
+
+    int *vetor = (int *)malloc(tam * sizeof(int)); 
     if (vetor == NULL) {
+        perror("Erro ao alocar memória");
         return; 
     }
 
-
-    if (cenario == 'A'){
-        int i;
+    // Preenchimento do vetor
+    if (cod_cenario == 'A'){
         int limite_superior = 1000001;
 
-        for (i = 0; i < tam; i++) {
+        for (int i = 0; i < tam; i++) {
             vetor[i] = rand() % limite_superior; 
         }
     }
 
-
-    else if (cenario == 'C'){
+    else if (cod_cenario == 'C'){
         for (int i = 0; i < tam; i++){
             vetor[i] = i;
         }
     }
 
-
-    else if (cenario == 'D'){
+    else if (cod_cenario == 'D'){
         for (int i = 0; i< tam; i++){
             vetor[i] = tam - i;
         }
     }
 
-    //chamar função escrita_binario
+    printf("%c", cod_cenario);
+    // Geração do nome do arquivo
+    const char *nome_tamanho = obter_nome_tam(cod_tam);
+    const char *nome_cenario = obter_nome_cenario(cod_cenario);
 
+    char nome_arquivo[100];
+    snprintf(nome_arquivo, sizeof(nome_arquivo),
+                "%s_%s.bin", nome_tamanho, nome_cenario);
+    
+    // Escrita no arquivo binário            
+    FILE *arquivo = fopen(nome_arquivo, "wb");
 
+    if(arquivo == NULL){
+        perror("Erro ao abrir o arquivo para escrita.");
+        free(vetor);
+        return;
+    }
+
+    fwrite(vetor, sizeof(int), tam, arquivo);
+
+    fclose(arquivo);
+    printf("Arquivo binário '%s' criado com sucesso com %d inteiros.\n", nome_arquivo, tam);
+
+    
     free(vetor);
 }
 
 
-void escrita_binario(int *conteudo, int tam, const char *nome_arquivo){
-    FILE *arquivo = fopen(nome_arquivo, "wb");
 
-    if (arquivo == NULL) {
-        perror("Erro ao abrir o arquivo para escrita.");
-        return;
-    }
+/*--------------- FUNÇÃO MAIN ---------------*/
 
-    size_t itens_escritos = fwrite(conteudo, sizeof(int), tam, arquivo);
+int main(){
+    srand(time(NULL));
 
-    if (itens_escritos != tam) {
-        fprintf(stderr, "Atenção: Apenas %zu de %d itens foram escritos.\n", itens_escritos, tam);
-    }
+    char tamanho;
+    char cenario;
 
-    fclose(arquivo);
+    printf("Digite o tamanho (P, M, G) e o cenario desejados (A, C, D)\n");
+    scanf(" %c %c", &tamanho, &cenario);
 
-    printf("Arquivo binário '%s' criado com sucesso com %d inteiros.\n", nome_arquivo, tam);
+    gerador_arquivo(tamanho, cenario);
+
+    return 0;
 }
